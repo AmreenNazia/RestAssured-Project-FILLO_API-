4889a8813ef17a9f5287887f01e890e14e91aa8c
AmreenNazia <156974279+AmreenNazia@users.noreply.github.com>
2024-10-01 19:51:29 -0500

    patient Module

--- src/test/java/stepdefinition/PatientSteps.java
+++ src/test/java/stepdefinition/PatientSteps.java
@@ -1,5 +1,48 @@
 package stepdefinition;
 
+
+import io.cucumber.java.en.*;
 public class PatientSteps {
+	@Given("User gets test data from the sheet for new patient  with scenario name {string}")
+	public void user_gets_test_data_from_the_sheet_for_new_patient_with_scenario_name(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User sends the HTTP POST request with the request body to create new patient")
+	public void user_sends_the_http_post_request_with_the_request_body_to_create_new_patient() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Patient is created with an password")
+	public void patient_is_created_with_an_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Patient logs in and patient token is generated")
+	public void patient_logs_in_and_patient_token_is_generated() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Given("User gets test data from the sheet for new patient  with scenario name is not {string}")
+	public void user_gets_test_data_from_the_sheet_for_new_patient_with_scenario_name_is_not(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+// Negative Scenarios
+	@When("User sends the HTTP POST request to create new patient")
+	public void user_sends_the_http_post_request_to_create_new_patient() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Status code is varified")
+	public void status_code_is_varified() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 
 }
